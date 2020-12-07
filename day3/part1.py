c26fe566cf009849dfc3ffa0313c0baa199edcc1
William Gibson <wgibson000@gmail.com>
2020-12-07 09:59:25 -0500

    Amend to answer original question

--- day3/part1.py
+++ day3/part1.py
@@ -3,18 +3,14 @@ with open("input.txt", "r") as input_file:
 
 index = 0
 count = 0
-line_count = 0
 for line in tree_map:
-    line_count += 1
-    if line_count % 2 != 0:
-        continue
     line = [c for c in line]
     if line[index] == "#":
         line[index] = 'X'
         count += 1
     else:
         line[index] = 'O'
-    index += 7
+    index += 3
     print(''.join(line))
     # we've gone over, so we need to reset the index
     # this simulates the forest "repeating"
